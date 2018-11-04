7632545b87459fd5f3067c11d2fde3c936da6b1c
Lee Aspitarte <aspitarte@gmail.com>
2018-11-04 02:51:53 -0800

    continued work converting times to np datetime64
    conversion to datetime for display is now done with a tick formatter,
    instead of converting the whole array
    
    
    Former-commit-id: b8108f34400409c3b53476db28c98fa5d1855d2e

--- eventlog.py
+++ eventlog.py
@@ -119,8 +119,8 @@ def geteventinfo(jsonfile, cuttimes = None ,eventstr = None):
     tci = {} #Should be using ordered dict instead to ensure time order?
     for event in jsonfile:
         if (event['event']['type'] == eventstr) or (eventstr == None):
-            ts = event['dt']
-            time = np.datetime64(int(ts),'s')
+            # ts = 
+            time = np.datetime64(int(event['dt']),'s')
             # time = datetime.datetime.utcfromtimestamp(event['dt'])
             # time = time.replace(tzinfo=pytz.utc)
             tci[time] = event['event']['event info']
