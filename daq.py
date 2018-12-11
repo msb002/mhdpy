4046b70251b68129dd844532517dd41c4491ae39
Lee Aspitarte <aspitarte@gmail.com>
2018-12-10 16:00:26 -0800

    fix lightfield bugs during experiment
    Former-commit-id: 65324f595e3de242dc4e26a257356d6c020216ee

--- daq.py
+++ daq.py
@@ -6,15 +6,10 @@ import datetime
 import win32com.client  # Python ActiveX Client
 import os
 
-<<<<<<< Updated upstream
-repopath = 'C:\\Users\\aspitarl\\Git\\MHDLab\\'
-=======
 repopath = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
->>>>>>> Stashed changes
 
 def gen_filepath(LabVIEW, devicename, extension = '.tdms',DAQmx = False, Logfile = False):
     vipath = os.path.join(repopath,'Common Subvis\\GenerateFilePaths.vi')
-    print(vipath)
     VI = LabVIEW.getvireference(vipath)  # Path to LabVIEW VI
     VI._FlagAsMethod("Run")
 
@@ -27,12 +22,14 @@ def gen_filepath(LabVIEW, devicename, extension = '.tdms',DAQmx = False, Logfile
     return filepath  
 
 def get_fileinfo(LabVIEW):
-    VI = LabVIEW.getvireference(repopath + 'Global Variables.vi')  # Path to LabVIEW VI
+    vipath = os.path.join(repopath,'Global Variables.vi')
+    VI = LabVIEW.getvireference(vipath)  # Path to LabVIEW VI
     fileinfo = VI.getcontrolvalue('Global File Information')  # Get return value
     return fileinfo  
 
 def get_rawdatafolder(LabVIEW):
-    VI = LabVIEW.getvireference(repopath + 'Global Variables.vi')  # Path to LabVIEW VI
+    vipath = os.path.join(repopath,'Global Variables.vi')
+    VI = LabVIEW.getvireference(vipath)  # Path to LabVIEW VI
     datafolder = VI.getcontrolvalue('Data folder')  # Get return value
     datestring = datetime.date.today().strftime("%Y-%m-%d")
     datafolder = os.path.join(datafolder,datestring)
