5804b70eddbe9369f8992a62d66352ac46f716fa
Lee Aspitarte <aspitarte@gmail.com>
2018-12-11 21:13:07 -0800

    fix finding of Acquisition folder

--- mhdpy/daq.py
+++ mhdpy/daq.py
@@ -6,10 +6,10 @@ import datetime
 import win32com.client  # Python ActiveX Client
 import os
 
-repopath = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+repopath = os.path.split(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0])[0]
 
 def gen_filepath(LabVIEW, devicename, extension = '.tdms',DAQmx = False, Logfile = False):
-    vipath = os.path.join(repopath,'Common Subvis\\GenerateFilePaths.vi')
+    vipath = os.path.join(repopath,'Acquisition\\Common Subvis\\GenerateFilePaths.vi')
     VI = LabVIEW.getvireference(vipath)  # Path to LabVIEW VI
     VI._FlagAsMethod("Run")
 
@@ -22,22 +22,18 @@ def gen_filepath(LabVIEW, devicename, extension = '.tdms',DAQmx = False, Logfile
     return filepath  
 
 def get_fileinfo(LabVIEW):
-    vipath = os.path.join(repopath,'Global Variables.vi')
+    vipath = os.path.join(repopath,'Acquisition\\Global Variables.vi')
     VI = LabVIEW.getvireference(vipath)  # Path to LabVIEW VI
     fileinfo = VI.getcontrolvalue('Global File Information')  # Get return value
     return fileinfo  
 
 def get_rawdatafolder(LabVIEW):
-    vipath = os.path.join(repopath,'Global Variables.vi')
+    vipath = os.path.join(repopath,'Acquisition\\Global Variables.vi')
     VI = LabVIEW.getvireference(vipath)  # Path to LabVIEW VI
     datafolder = VI.getcontrolvalue('Data folder')  # Get return value
     datestring = datetime.date.today().strftime("%Y-%m-%d")
     datafolder = os.path.join(datafolder,datestring)
     return datafolder
-
-def test():
-    print('hello')
-
 
 # LabVIEW = win32com.client.Dispatch("Labview.Application")
 # print(get_rawdatafolder(LabVIEW))
